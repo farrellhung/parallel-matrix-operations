0a06e3e0d4c79fa03b11a08695e7932e771eb352
Farrell Hung <91648049+farrellhung@users.noreply.github.com>
2023-11-13 13:30:09 +0800

    added the naive approach for comparison

--- pmo.c
+++ pmo.c
@@ -15,7 +15,6 @@
 
 /**
  * struct Matrix
- * Writen by: Farrell Hung (56691108)
  * 
  * A Matrix struct to help keep track of the matrix size in addition to the matrix value.
 */
@@ -32,32 +31,28 @@ typedef struct Matrix Matrix;
 
 /**
  * create_matrix(int n_rows, int n_cols)
- * Writen by: Farrell Hung (56691108)
  * 
  * Create an empty matrix of size n_rows by n_cols. Allocates memory dynamically.
  * 
  * Parameters:
- * n_rows: length of the matrix row
- * n_cols: length of the matrix column
+ * row_length: length of the matrix row
+ * col_length: length of the matrix column
  * 
  * Returns:
  * Matrix* of the generated empty matrix.
 */
-Matrix* create_matrix(int n_rows, int n_cols) {
+Matrix* create_matrix(int row_length, int col_length) {
   struct Matrix* matrix = malloc(sizeof(matrix));
-  matrix -> row_length = n_rows;
-  matrix -> col_length = n_cols;
-  int** data = malloc(sizeof(int*)*n_rows);
-  for(int i = 0; i < n_rows; i++) {
-    data[i] = calloc(n_cols, sizeof(int));
-  }
+  matrix -> row_length = row_length;
+  matrix -> col_length = col_length;
+  int** data = malloc(sizeof(int*)*row_length);
+  for(int i = 0; i < row_length; i++) data[i] = calloc(col_length, sizeof(int));
   matrix -> data = data;
   return matrix;
 }
 
 /**
  * init_matrix(int n_rows, int n_cols)
- * Writen by: Farrell Hung (56691108)
  * 
  * Initialize a matrix by first creating an empty matrix of size n_rows by n_cols, done by calling create_matrix.
  * Then, scan for input and put the value inside matrix -> data.
@@ -70,21 +65,18 @@ Matrix* create_matrix(int n_rows, int n_cols) {
  * Returns:
  * Matrix* of the initialized matrix.
 */
-Matrix* init_matrix(int n_rows, int n_cols) {
-  struct Matrix* matrix = create_matrix(n_rows, n_cols);
-  for(int i = 0; i < n_rows; i++) {
-    for(int j = 0; j < n_cols; j++) {
-      scanf(" %d", &(matrix -> data[i][j]));
-    }
+Matrix* init_matrix(int row_length, int col_length) {
+  struct Matrix* matrix = create_matrix(row_length, col_length);
+  for(int i = 0; i < row_length; i++) {
+    for(int j = 0; j < col_length; j++) scanf(" %d", &(matrix -> data[i][j]));
   }
   return matrix;  
 }
 
 /**
  * print_matrix(Matrix* matrix)
- * Writen by: Farrell Hung (56691108)
  * 
- * Simply prints out the matrix by looping over the data.
+ * Prints out the matrix by looping over the data.
  * 
  * Parameters:
  * matrix: the matrix to be printed
@@ -92,15 +84,13 @@ Matrix* init_matrix(int n_rows, int n_cols) {
 void print_matrix(Matrix* matrix) {
   for(int i = 0; i < matrix -> row_length; i++) {
     printf("%s", "\n");
-    for(int j = 0; j < matrix -> col_length; j++) {
-      printf("%d\t", matrix -> data[i][j]);
-    }
+    for(int j = 0; j < matrix -> col_length; j++) printf("%d\t", matrix -> data[i][j]);
   }
+  return;
 }
 
 /**
  * multiply_matrix(Matrix* a, Matrix* b)
- * Writen by: Farrell Hung (56691108)
  * 
  * Multiply 2 matrices. Only uses block and does not use any blackmagicfuckery algorithms for now.
  * 
@@ -120,21 +110,15 @@ Matrix* multiply_matrix(Matrix* a, Matrix* b) {
   int p = b -> col_length;
 
   Matrix* c = create_matrix(n, p);
-  int sum;
   int bsize = 2;
 
   for (int jj = 0; jj < p; jj += bsize) {
-    // for (int i = 0; i < n; i++)
-    //   for (int j = jj; j < min(jj+bsize,p); j++)
-    //     c -> data[i][j] = 0;
     for (int kk = 0; kk < m; kk += bsize) {
       for (int i = 0; i < n; i++) {
         for (int j = jj; j < min(jj+bsize,p); j++) {
-          sum = 0;
-          for (int k = kk; k < min(kk+bsize,m); k++) {
-            sum += a -> data[i][k] * b -> data[k][j];
-          }
-          (c -> data[i][j]) += sum;
+          int sum = 0;
+          for (int k = kk; k < min(kk+bsize,m); k++) sum += a -> data[i][k] * b -> data[k][j];
+          c -> data[i][j] += sum;
         }
       }
     }
@@ -143,8 +127,40 @@ Matrix* multiply_matrix(Matrix* a, Matrix* b) {
 }
 
 /**
+ * multiply_matrix_naive(Matrix* a, Matrix* b)
+ * 
+ * Multiply 2 matrices using the naive approach.
+ * 
+ * Parameters:
+ * a: left operand matrix
+ * b: right operand matrix
+ * 
+ * Returns:
+ * Matrix* of the resulted matrix operation.
+*/
+Matrix* multiply_matrix_naive(Matrix* a, Matrix* b) {
+  // ROW OF A = N (iterate as i), COLUMN OF A = M (iterate as k)
+  // ROW OF B = M (iterate as k), COLUMN OF B = P (iterate as j)
+
+  int n = a -> row_length;
+  int m = a -> col_length; // Expect a -> col_length to be equal to b -> row_length;
+  int p = b -> col_length;
+
+  Matrix* c = create_matrix(n, p);
+  int bsize = 2;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < p; j++) {
+      int sum = 0;
+      for (int k = 0; k < m; k++) sum += a -> data[i][k] * b -> data[k][j];
+      c -> data[i][j] += sum;
+    }
+  }
+  return c;
+}
+
+/**
  * min
- * Writen by: Farrell Hung (56691108)
  * 
  * Returns the lowest value among the two given integers.
  * 
@@ -168,9 +184,9 @@ int main() {
   //   printf(buf);
   // }
 
-  Matrix* a = init_matrix(2,3);
-  Matrix* b = init_matrix(3,2);
-  Matrix* c = multiply_matrix(a, b);
+  Matrix* a = init_matrix(3,3);
+  Matrix* b = init_matrix(3,3);
+  Matrix* c = multiply_matrix_naive(a, b);
   print_matrix(c);
   // Tried using 2x2 * 2x2, 2x3 * 3x2, and 3x3 * 3x3 works nicely.
   // Need to try using large matrix later. bsize needs to be optimized according to the server's cache size.
